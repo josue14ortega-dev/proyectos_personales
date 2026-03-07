30aa9872fc63b3ca3e5f9c81f527465c00c18ebf
Josue <josue14ortega@gmail.com>
2026-03-07 17:30:04 -0600

    Automatizacion

--- Proyectos personales/automatizacion_sheets.py
+++ Proyectos personales/automatizacion_sheets.py
@@ -1,23 +1,49 @@
+""" Automatizacion de Sheets
+Nombre: Josue
+Fecha: 6-Marzo-2026
+"""
+
+
 import pandas as pd
 
-# 1. Cargar los datos del CSV que bajaste de Sheets
 df = pd.read_csv("ventas - Hoja 1.csv")
-# 2. Tu lógica de ayer aplicada a una columna entera
-# Vamos a crear una columna nueva llamada 'Aumento'
+
 def calcular_ajuste(precio):
     if precio < 50:
         return precio * 0.15
     else:
         return 0
 
-# Aplicamos la función a la columna 'Precio'
+
 df['Aumento'] = df['Precio'].apply(calcular_ajuste)
 
-# 3. Calcular la ganancia total para el reporte
+
 total_ganancia = df['Aumento'].sum()
 
-# 4. Guardar el resultado en un nuevo Excel/CSV
 df.to_csv('reporte_final_sheets.csv', index=False)
 
 print(f"¡Hecho! Se procesaron {len(df)} productos.")
 print(f"La ganancia extra total es de: ${total_ganancia:.2f}")
+
+import matplotlib.pyplot as plt
+
+# 1. Preparar los datos para el gráfico
+nombres_productos = df['Producto']
+aumentos = df['Aumento']
+
+colores_inteligentes = ["limegreen" if a > 0 else "lightgrey" for a in df["Aumento"] ]
+
+# 2. Crear el gráfico de barras
+plt.figure(figsize=(10, 6))
+plt.bar(nombres_productos, aumentos, color='skyblue')
+
+# 3. Personalizarlo (Esto es lo que el cliente valora)
+plt.title('Impacto Económico por Producto')
+plt.xlabel('Productos')
+plt.ylabel('Ganancia Extra ($)')
+plt.xticks(rotation=45) # Rota los nombres para que se lean bien
+
+# 4. Guardar la imagen
+plt.tight_layout()
+plt.savefig('REPORTE DE OPTIMIZACIÓN FINANCIERA.png')
+print("¡Gráfico generado con éxito como 'REPORTE DE OPTIMIZACIÓN FINANCIERA.png'!")
