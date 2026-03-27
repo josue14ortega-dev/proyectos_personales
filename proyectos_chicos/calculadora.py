a9ebab7f2f3fb21e1757ebdaa3f1f53fc6920a7f
Josue <josue14ortega@gmail.com>
2026-03-27 09:55:35 -0600

    Calculadora de divisas

--- proyectos_chicos/calculadora.py
+++ proyectos_chicos/calculadora.py
@@ -6,17 +6,17 @@ Nombre: Josue
 Fecha: 21-Marzo-2026
 """
 
-
+#Se añade el float para si un numero tiene decimal, y el input para que el usuario ponga el numero.
 num1 = float(input('Ponga un numero: '))
 num2 = float(input('Ponga un numero: '))
-
+#Este es el menu de las opciones que hay.
 print('1. Suma')
 print('2. Resta')
 print('3. Multiplicacion')
 print('4. Division')
-
+#Aqui esta variable, manda un mensaje y gracias al input se puede seleccionar.
 opcion = input('Escribe un numero de la opcion (1/2/3/4): ')
-
+#Dependiendo la eleccion del usuario, se hacedicha operacion, aqui entra en juego los if, elif y else
 resultado = 0
 if opcion == '1':
     resultado = num1 + num2
@@ -24,8 +24,7 @@ elif opcion == '2':
     resultado = num1 - num2
 elif opcion == '3':
     resultado = num1 * num2
-elif opcion == '4':
-    
+elif opcion == '4': #Dentro de este else se añade otras condicionales para lograr el resultado adecuado
     if num2 == 0:
         print('Nose puede dividir el 0')
     else:
@@ -34,4 +33,4 @@ elif opcion == '4':
 else:
     print('Opcion no valida')
     
-print(f'El resultado es: {resultado}')
+print(f'El resultado es: {resultado}')# el f'strings se usa para poder poner los {} sin que haya problema
---
+++ proyectos_chicos/divisas.py
@@ -0,0 +1,31 @@
+"""
+CALCULADORA DE DIVISAS
+    Holaa, este es un mini proyecto en el cual hago una calculadora
+    de divisas con python con el objetivo de afinar mis habilidades y logica.
+Nombre: Josue
+Fecha: 27-Marzo-2026
+"""
+pregunta = float(input('¿A cuanto esta el dolar hoy? '))
+VALOR_USD = pregunta 
+print('1. Dolares')
+print('2. Pesos')
+
+cambio = input('Escribe un numero (1/2): ')
+cantidad = float(input('Cantidad que desea convertir: '))
+
+resultado = 0
+if cambio == '1':
+    resultado = cantidad * VALOR_USD
+    
+    
+elif cambio =='2':
+    resultado = cantidad/VALOR_USD
+    
+    
+else:
+    print('asi no es papi')
+    
+
+print(f'El resultado es: {resultado:.2f}')
+    
+    
