e65c73eb6f648d9d41c60a9c006ee417b3208062
Josue <josue14ortega@gmail.com>
2026-04-01 23:25:47 -0600

    Simulador de ahorro

---
+++ proyectos_chicos/finanzas.py
@@ -0,0 +1,16 @@
+saldo_actual = float(input('ingrese el saldo que tienes hoy: '))
+tasa_anual = float(input('Cual es su tasa anual en su banco: '))
+meta = float(input('¿Cual es el precio? '))
+
+tasa_diaria = (tasa_anual / 100) / 365
+if tasa_anual <= 0:
+    print('La tasa anual debe ser mayor a 0')
+else:
+    dias = 0
+    while saldo_actual < meta:
+        ganancia = saldo_actual * (tasa_diaria)
+        saldo_actual += ganancia
+        dias += 1
+    meses = dias // 30
+    dias_restantes = dias % 30
+    print(f'Josue, te faltan {meses} meses y { dias} dias ')
